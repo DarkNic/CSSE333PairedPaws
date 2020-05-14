1549f375d90cd5900629c9975b89e48750d01a0a
haveaniceday33 <munrosamuel@gmail.com>
2020-05-14 11:01:34 -0400

    Deleting accounts backend. Need frontend to fully implement

--- src/ApplicationLogin.java
+++ src/ApplicationLogin.java
@@ -117,4 +117,36 @@ public class ApplicationLogin {
 		}
 		return enc.encodeToString(hash);
 	}
+	
+	public static boolean deleteAccount(String uname) {
+		int ret = 0;
+		try {
+			CallableStatement cs = Main.con.getConnection().prepareCall("{? = call Remove_Person(?)}");
+			cs.registerOutParameter(1, Types.INTEGER);
+			cs.setString(2, uname);
+			cs.execute();
+			ret = cs.getInt(1);
+			if(ret != 0)
+				throw new SQLException();
+			cs.close();
+			ret = 0;
+			CallableStatement cs2 = Main.con.getConnection().prepareCall("{? = call Remove_Contact(?)}");
+			cs2.registerOutParameter(1, Types.INTEGER);
+			cs2.setString(2, uname);
+			cs2.execute();
+			ret = cs2.getInt(1);
+			if(ret != 0)
+				throw new SQLException();
+			cs2.close();
+			return true;
+		} catch (SQLException e) {
+			String error = "An error occurred.";
+			if(ret == 1)
+				error = "Must be logged in to delete an account";
+			
+			JOptionPane.showMessageDialog(null, error);
+		}
+		
+		return false;
+	}
 }
